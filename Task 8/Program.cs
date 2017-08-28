4df2b047c05689b5ac65dd4a54027ed980943566
Анна <@LAPTOP-63K8I5TL>
2017-08-28 22:05:31 +0500

    Ввод

--- Task 8/Program.cs
+++ Task 8/Program.cs
@@ -10,6 +10,14 @@ namespace Task_8
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Введите количество вершин графов");
+            int n = int.Parse(Console.ReadLine());
+            int[,] GraphA = new int[n, n];
+            int[,] GraphB = new int[n, n];
+            // Ввод матриц смежности
+
+            int [,]
+
         }
     }
 }
