8659099b2fff24ccd5c4062bdb5a76ce2ea47862
Анна <@LAPTOP-63K8I5TL>
2017-09-03 21:57:49 +0500

    Полное решение

--- Task 8/Program.cs
+++ Task 8/Program.cs
@@ -8,75 +8,94 @@ namespace Task_8
 {
     class Program
     {
-        static void GeneratorTests(out int[,] GraphA, out int[,] GraphB, out int n)
+        /// <summary>
+        /// Генератор тестов
+        /// </summary>
+        /// <param name="GraphA">Граф A</param>
+        /// <param name="GraphB">Граф B</param>
+        /// <param name="size">Размер матриц</param>
+        static void GeneratorTests(out int[,] GraphA, out int[,] GraphB, out int size)
         {
             Random a = new Random();
-            n = a.Next(2, 11);
-            GraphA = new int[n, n];
+            // Задаем размер матриц
+            size = a.Next(2, 11);
+
+            // Генерируем граф A
+            GraphA = new int[size, size];
             Console.WriteLine("Граф А:");
-            for (int i = 0; i < n; i++)
+            for (int i = 0; i < size; i++)
             {
-                for (int j = 0; j < n; j++)
+                for (int j = 0; j < size; j++)
                 {
+                    // Главная диагональ - нули
                     if (i == j)
                         GraphA[i, j] = 0;
+                    // Выше главной диагонали - генерируем ребра
                     else if (i < j)
                         GraphA[i, j] = a.Next(2);
+                    // Ниже главной диагонали - используем уже созданные ребра 
                     else GraphA[i, j] = GraphA[j, i];
                     Console.Write("{0} ", GraphA[i, j]);
                 }
                 Console.WriteLine();
             }
 
+            // Генерируем изоморфный граф B
             Console.WriteLine("Граф B:");
-            GraphB = new int[n, n];
-            int[] peak = new int[n];
-            for (int i = 0; i < n; i++)
+            GraphB = new int[size, size];
+
+            // Создаем случайное биективное соответствие между вершинами графов
+            int[] peak = new int[size];
+            for (int i = 0; i < size; i++)
                 peak[i] = -1;
-            for (int i = 0; i < n; i++)
+            for (int i = 0; i < size; i++)
                 while (true)
                 {
-                    int place = a.Next(n);
+                    int place = a.Next(size);
                     if (peak[place] == -1)
                     {
                         peak[place] = i;
                         break;
                     }
                 }
-            for (int i = 0; i < n; i++)
+
+            // Составляем матрицу смежности для графа B в соответствии с матрицей графа A
+            for (int i = 0; i < size; i++)
             {
-                for (int j = 0; j < n; j++)
+                for (int j = 0; j < size; j++)
                 {
                     GraphB[i, j] = GraphA[peak[i], peak[j]];
                     Console.Write("{0} ", GraphB[i, j]);
                 }
                 Console.WriteLine();
             }
-            Console.WriteLine("Ожидаемый результат:");
-            for (int i = 0; i < n; i++)
-                Console.Write("{0} ", peak[i] + 1);
-            Console.WriteLine();
+
         }
 
-
-
-
-        static bool NextPermutation(ref int[] a)
+        /// <summary>
+        /// Генерация перестановок
+        /// </summary>
+        /// <param name="peak">Массив для варианта соответствия вершин</param>
+        /// <returns></returns>
+        static bool NextPermutation(ref int[] peak)
         {
-            for (var n = a.Length - 1; ;)
+            int index = peak.Length - 1;
+            while (true)
             {
-                var n1 = n;
-                if (a[--n] < a[n1])
+                int n = index;
+                index--;
+                if (peak[index] < peak[n])
                 {
-                    var m = a.Length;
-                    while (a[n] >= a[--m]) ;
-                    var t = a[n]; a[n] = a[m]; a[m] = t;
-                    Array.Reverse(a, n1, a.Length - n1);
+                    int m = peak.Length - 1;
+                    while (peak[index] >= peak[m])
+                        m--;
+                    int t = peak[index]; peak[index] = peak[m]; peak[m] = t;
+                    Array.Reverse(peak, n, peak.Length - n);
                     return true;
                 }
-                if (n == 0)
+                if (index == 0)
                 {
-                    Array.Reverse(a);
+                    Array.Reverse(peak);
                     return false;
                 }
             }
@@ -85,39 +104,52 @@ namespace Task_8
 
         static void Main(string[] args)
         {
-            int[,] GraphA, GraphB;
-            int n;
+            int[,] GraphA, GraphB; int n;                 // Матрицы смежности и кол-во вершин
+            // Генерируем матрицы смежности
             GeneratorTests(out GraphA, out GraphB, out n);
+            // Граф для проверки наличия биективного соответствия
             int[,] graph = new int[n, n];
 
+            // Массив для варианта соответствия вершин 
             int[] a = new int[n];
             for (int i = 0; i < n; i++)
                 a[i] = i;
 
-            bool por = false;
-            while (!por)
+            bool ok = false;
+            // Генерируем перестановку и проверяем, совпадают ли матрицы смежности
+            while (!ok)
             {
-                por = true;
-                bool ok = NextPermutation(ref a);
+                ok = true;
+                bool result = NextPermutation(ref a);   // Генерация новой перестановки вершин
+                // Составляем матрицу смежности по новой последовательности вершин и сравниваем с матрицей графа A
                 for (int i = 0; i < n; i++)
+                {
                     for (int j = 0; j < n; j++)
                     {
                         graph[i, j] = GraphB[a[i], a[j]];
-                        if (graph[i, j] != GraphA[i, j]) por = false;
+                        if (graph[i, j] != GraphA[i, j])
+                        {
+                            ok = false;
+                            break;
+                        }
                     }
+                    if (!ok) break;
+                }
             }
 
-            for (int i = 0; i < n; i++)
-                Console.Write("{0} ", a[i] + 1);
-            Console.WriteLine();
-
-            Console.WriteLine("Измененный граф: ");
+            // Граф, полученный в результате изменения порядка вершин
+            Console.WriteLine("Измененный граф (для проверки соответствия): ");
             for (int i = 0; i < n; i++)
             {
                 for (int j = 0; j < n; j++)
                     Console.Write("{0} ", graph[i, j]);
                 Console.WriteLine();
             }
+
+            // Результат
+            Console.WriteLine("Биективное соответствие (вершина графа A =  вершина графа B):");
+            for (int i = 0; i < n; i++)
+                Console.WriteLine("{0} = {1} ", i + 1, a[i] + 1);           
             Console.ReadLine();
         }
     }
